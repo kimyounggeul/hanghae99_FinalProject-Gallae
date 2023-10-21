0fe3cc7a5b45be508fd003eed7a2d47136dbf105
kimyounggeul <129300980+kimyounggeul@users.noreply.github.com>
2023-10-21 18:55:52 +0900

    refactor : time
    한국 시간에 맞춤

--- src/main/java/com/sparta/team2project/comments/service/CommentsService.java
+++ src/main/java/com/sparta/team2project/comments/service/CommentsService.java
@@ -14,12 +14,13 @@ import com.sparta.team2project.posts.repository.PostsRepository;
 import com.sparta.team2project.users.Users;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
