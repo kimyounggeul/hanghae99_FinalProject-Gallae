9cee6dfd279bdf4ab998e1525733b42040cdde96
kimyounggeul <129300980+kimyounggeul@users.noreply.github.com>
2023-10-26 13:12:27 +0900

    refactor - 버셀 주소 CORS 추가

--- src/main/java/com/sparta/team2project/commons/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/team2project/commons/config/WebSecurityConfig.java
@@ -62,7 +62,7 @@ public class WebSecurityConfig {
     public CorsConfigurationSource configurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://gallae-fe.vercel.app"));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         //configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
