c7838efacf4e2f1dc73c10b050cceb36433663bf
seungtaemoon <Robbie@email.com>
2023-11-01 17:21:01 +0900

    세부일정 사진 (Pictures) 관련 기능 수정 - POST 사진 한 개씩만 등록 - POST/PUT Resize 없이 반환 - POST/GET/PUT 사진 URL만 반환

--- src/main/java/com/sparta/team2project/pictures/controller/PicturesController.java
+++ src/main/java/com/sparta/team2project/pictures/controller/PicturesController.java
@@ -1,19 +1,13 @@
 package com.sparta.team2project.pictures.controller;
 
 import com.sparta.team2project.commons.dto.MessageResponseDto;
-import com.sparta.team2project.commons.exceptionhandler.CustomException;
-import com.sparta.team2project.commons.exceptionhandler.ErrorCode;
 import com.sparta.team2project.commons.security.UserDetailsImpl;
-import com.sparta.team2project.pictures.dto.PicturesMessageResponseDto;
-import com.sparta.team2project.pictures.dto.PicturesResponseDto;
 import com.sparta.team2project.pictures.dto.UploadResponseDto;
 import com.sparta.team2project.pictures.service.PicturesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -23,18 +17,11 @@ public class PicturesController {
     private final PicturesService picturesService;
 
     @PostMapping("/schedules/{schedulesId}/pictures")
-    public UploadResponseDto uploadPictures(@PathVariable("schedulesId") Long schedulesId,
-                                            @RequestPart("file") List<MultipartFile> files,
+    public String uploadPictures(@PathVariable("schedulesId") Long schedulesId,
+                                            @RequestParam("file") MultipartFile file,
                                             @AuthenticationPrincipal UserDetailsImpl userDetails
                                             ){
-        // 업로드할 사진이 3개를 초과하면 예외 출력
-        if(files.size() > 3){
-            throw new CustomException(ErrorCode.EXCEED_PICTURES_LIMIT);
-        }
-        // 그 외의 경우 업로드 수행
-        else{
-            return picturesService.uploadPictures(schedulesId, files, userDetails.getUsers());
-        }
+            return picturesService.uploadPictures(schedulesId, file, userDetails.getUsers());
     }
 
     @GetMapping("/schedules/{schedulesId}/pictures")
@@ -43,12 +30,12 @@ public class PicturesController {
     }
 
     @GetMapping("/pictures/{picturesId}")
-    public PicturesResponseDto getPicture(@PathVariable("picturesId") Long picturesId){
+    public String getPicture(@PathVariable("picturesId") Long picturesId){
         return picturesService.getPicture(picturesId);
     }
 
     @PutMapping("/pictures/{picturesId}")
-    public PicturesMessageResponseDto updatePictures(@PathVariable("picturesId") Long picturesId,
+    public String updatePictures(@PathVariable("picturesId") Long picturesId,
                                                      @RequestParam("file") MultipartFile file,
                                                      @AuthenticationPrincipal UserDetailsImpl userDetails
                                           ){
