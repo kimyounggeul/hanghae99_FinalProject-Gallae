acc9cd7ee4f19f89fe2220636937416ab8563b81
kimyounggeul <129300980+kimyounggeul@users.noreply.github.com>
2023-11-02 20:05:13 +0900

    clear

--- src/main/java/com/sparta/team2project/commons/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.team2project.commons.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaConfig {
-}
