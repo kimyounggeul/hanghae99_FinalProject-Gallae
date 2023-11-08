a0b66853f2a8d760d7a1594818f5a5b303139911
seungtaemoon <Robbie@email.com>
2023-11-08 22:09:10 +0900

    S3 사진 업로드 기능 테스트 코드 수정 - 게시글 사진 등록, 조회, 수정, 삭제 기능 테스트 수정 - 세부일정 사진 등록, 조회, 수정, 삭제 기능 테스트 보완

--- src/test/java/com/sparta/team2project/postspictures/PostsPicturesServiceTest.java
+++ src/test/java/com/sparta/team2project/postspictures/PostsPicturesServiceTest.java
@@ -1,15 +1,16 @@
-package com.sparta.team2project.pictures;
+package com.sparta.team2project.postspictures;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.S3Object;
 import com.sparta.team2project.commons.entity.UserRoleEnum;
-import com.sparta.team2project.pictures.entity.Pictures;
-import com.sparta.team2project.pictures.repository.PicturesRepository;
 import com.sparta.team2project.posts.dto.TripDateOnlyRequestDto;
 import com.sparta.team2project.posts.entity.PostCategory;
 import com.sparta.team2project.posts.entity.Posts;
+import com.sparta.team2project.posts.entity.PostsPictures;
+import com.sparta.team2project.posts.repository.PostsPicturesRepository;
+import com.sparta.team2project.posts.repository.PostsRepository;
 import com.sparta.team2project.schedules.dto.CreateSchedulesRequestDto;
 import com.sparta.team2project.schedules.dto.SchedulesRequestDto;
 import com.sparta.team2project.schedules.entity.Schedules;
@@ -35,14 +36,16 @@ import static org.mockito.Mockito.when;
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.S3;
 
 @Testcontainers
-public class PicturesServiceTest {
+public class PostsPicturesServiceTest {
     private static final DockerImageName LOCALSTACK_IMAGE = DockerImageName.parse("localstack/localstack:latest");
 
     @Container
     LocalStackContainer localStackContainer = new LocalStackContainer(LOCALSTACK_IMAGE)
             .withServices(S3);
 
-    private PicturesRepository picturesRepository;
+    private PostsRepository postsRepository;
+
+    private PostsPicturesRepository postsPicturesRepository;
 
     @Before
     public AmazonS3 setUpS3() {
@@ -57,7 +60,8 @@ public class PicturesServiceTest {
 
     @BeforeEach
     public void setUp() {
-        picturesRepository = Mockito.mock(PicturesRepository.class);
+        postsRepository = Mockito.mock(PostsRepository.class);
+        postsPicturesRepository = Mockito.mock(PostsPicturesRepository.class);
     }
 
     public Users MockUsers(){
@@ -105,66 +109,69 @@ public class PicturesServiceTest {
         return new Schedules(MockTripDate(), MockSchedulesRequestDto());
     }
 
-    public Pictures MockPictures(){
-        return new Pictures(MockSchedules(), "image/test.png", "test.png", "image/png", 100000L);
+    public PostsPictures MockPostsPictures(){
+        return new PostsPictures(MockPosts(), "image/test.png", "test.png", "image/png", 100000L);
     }
 
-    public Pictures MockPicturesTwo(){
-        return new Pictures(MockSchedules(), "image/test2.jpg", "test2.jpg", "image/jpg", 200000L);
+    public PostsPictures MockPostsPicturesTwo(){
+        return new PostsPictures(MockPosts(), "image/test2.jpg", "test2.jpg", "image/jpg", 200000L);
     }
-
 
     @Test
-    void testUploadAndGetPictures(){
+    void testUploadAndGetPostsPictures(){
         AmazonS3 amazonS3 = setUpS3();
-        Schedules schedules = MockSchedules();
-        List<Pictures> picturesList = schedules.getPicturesList();
-        Pictures pictures = MockPictures();
+        Posts posts = MockPosts();
+        Users users = MockUsers();
+        List<PostsPictures> postsPicturesList = posts.getPostsPicturesList();
+        PostsPictures postsPictures = MockPostsPictures();
         for(int i = 0; i < 3; i++){
-            picturesList.add(pictures);
+            postsPicturesList.add(postsPictures);
         }
 
+        List<Posts> postsList = new ArrayList<>();
+        postsList.add(posts);
+
         // 입력한 사진 파일이 등록되는 URL값 출력
-        when(picturesRepository.saveAll(picturesList)).thenReturn(picturesList);
-        when(picturesRepository.findAllBySchedules(schedules)).thenReturn(schedules.getPicturesList());
+        when(postsPicturesRepository.saveAll(postsPicturesList)).thenReturn(postsPicturesList);
+        when(postsRepository.findByUsers(users)).thenReturn(postsList);
+        when(postsPicturesRepository.findByPosts(posts)).thenReturn(posts.getPostsPicturesList());
         System.out.println("등록할 사진 파일 링크");
-        for(Pictures picture: picturesList){
-            System.out.println("사진 파일 링크: " + picture.getPicturesURL());
+        for(PostsPictures postPicture: postsPicturesList){
+            System.out.println("사진 파일 링크: " + postPicture.getPostsPicturesURL());
         }
 
         // 등록 가능한 최대 사진 수 확인
-        System.out.println("등록가능한 최대 사진 수: " + picturesList.size());
-        assertTrue(picturesList.size() <= 3);
+        System.out.println("등록가능한 최대 사진 수: " + postsPicturesList.size());
+        assertTrue(postsPicturesList.size() <= 3);
 
         // Repository등록 사진 정보 출력
-        List<Pictures> picturesSavedRepository = picturesRepository.saveAll(picturesList);
+        List<PostsPictures> picturesSavedRepository = postsPicturesRepository.saveAll(postsPicturesList);
 
-        for(Pictures picture: picturesSavedRepository){
-            System.out.println("저장 사진 파일 링크: " + picture.getPicturesURL());
+        for(PostsPictures postPicture: picturesSavedRepository){
+            System.out.println("저장 사진 파일 링크: " + postPicture.getPostsPicturesURL());
         }
 
         System.out.println("Repository 저장한 최대 사진 수: " + picturesSavedRepository.size());
         assertTrue(picturesSavedRepository.size() <= 3);
-        assertEquals(picturesSavedRepository.get(0).getPicturesURL(), picturesList.get(0).getPicturesURL());
+        assertEquals(picturesSavedRepository.get(0).getPostsPicturesURL(), postsPicturesList.get(0).getPostsPicturesURL());
 
         // Repository추출 사진 정보 출력
-        List<Pictures> picturesFromRepository = picturesRepository.findAllBySchedules(schedules);
+        List<PostsPictures> picturesFromRepository = postsPicturesRepository.findByPosts(posts);
 
-        for(Pictures picture: picturesFromRepository){
-            System.out.println("추출 사진 파일 링크: " + picture.getPicturesURL());
+        for(PostsPictures postsPicture: picturesFromRepository){
+            System.out.println("추출 사진 파일 링크: " + postsPicture.getPostsPicturesURL());
         }
 
         System.out.println("Repository 추출한 최대 사진 수: " + picturesFromRepository.size());
         assertTrue(picturesFromRepository.size() <= 3);
-        assertEquals(picturesFromRepository.get(0).getPicturesURL(), picturesSavedRepository.get(0).getPicturesURL());
-
+        assertEquals(picturesFromRepository.get(0).getPostsPictureSize(), picturesSavedRepository.get(0).getPostsPictureSize());
 
         // S3 사진 등록 기능 확인
         String bucketName = "foo";
         amazonS3.createBucket(bucketName);
         System.out.println(bucketName +" 버킷 생성");
 
-        String key = picturesFromRepository.get(0).getPicturesName();
+        String key = picturesFromRepository.get(0).getPostsPicturesName();
         String content = "테스트 사진";
         amazonS3.putObject(bucketName, key, content);
         System.out.println("파일을 업로드하였습니다. 파일 이름=" + key +", 파일 내용=" + content);
@@ -176,35 +183,40 @@ public class PicturesServiceTest {
     }
 
     @Test
-    void testUpdatePictures(){
+    void testUpdatePostsPictures(){
         AmazonS3 amazonS3 = setUpS3();
-        Pictures pictures = MockPictures();
-        Pictures picturesTwo = MockPicturesTwo();
-        Schedules schedules = MockSchedules();
+        PostsPictures postsPictures = MockPostsPictures();
+        PostsPictures postsPicturesTwo = MockPostsPicturesTwo();
+        Posts posts = MockPosts();
+        Users users = MockUsers();
 
-        List<Pictures> picturesList = schedules.getPicturesList();
+        List<Posts> postsList = new ArrayList<>();
+        postsList.add(posts);
 
-        when(picturesRepository.saveAll(picturesList)).thenReturn(picturesList);
-        when(picturesRepository.findAllBySchedules(schedules)).thenReturn(schedules.getPicturesList());
+        List<PostsPictures> postsPicturesList = posts.getPostsPicturesList();
+
+        when(postsPicturesRepository.saveAll(postsPicturesList)).thenReturn(postsPicturesList);
+        when(postsRepository.findByUsers(users)).thenReturn(postsList);
+        when(postsPicturesRepository.findByPosts(posts)).thenReturn(posts.getPostsPicturesList());
 
         // 첫번 째 사진 저장
-        picturesList.add(pictures);
+        postsPicturesList.add(postsPictures);
 
         // 첫번째 사진 저장 결과 출력
-        List<Pictures> picturesListSavedRepository = picturesRepository.saveAll(picturesList);
-        Pictures picturesSavedRepository = picturesListSavedRepository.get(0);
-        Pictures picturesFromRepository = picturesRepository.findAllBySchedules(schedules).get(0);
+        List<PostsPictures> picturesListSavedRepository = postsPicturesRepository.saveAll(postsPicturesList);
+        PostsPictures picturesSavedRepository = picturesListSavedRepository.get(0);
+        PostsPictures picturesFromRepository = postsPicturesRepository.findByPosts(posts).get(0);
 
-        assertEquals(picturesSavedRepository.getPicturesURL(), picturesFromRepository.getPicturesURL());
-        System.out.println("최초 저장된 사진 URL: " + picturesSavedRepository.getPicturesURL());
-        System.out.println("추출된 사진 URL: " + picturesFromRepository.getPicturesURL());
+        assertEquals(picturesSavedRepository.getPostsPicturesURL(), picturesFromRepository.getPostsPicturesURL());
+        System.out.println("최초 저장된 사진 URL: " + picturesSavedRepository.getPostsPicturesURL());
+        System.out.println("추출된 사진 URL: " + picturesFromRepository.getPostsPicturesURL());
 
         // S3 첫 번째 사진 등록 기능 확인
         String bucketName = "foo";
         amazonS3.createBucket(bucketName);
         System.out.println(bucketName +" 버킷 생성");
 
-        String key = picturesFromRepository.getPicturesName();
+        String key = picturesFromRepository.getPostsPicturesName();
         String content = "테스트 사진1";
         amazonS3.putObject(bucketName, key, content);
         System.out.println("파일을 업로드하였습니다. 파일 이름=" + key +", 파일 내용=" + content);
@@ -214,18 +226,18 @@ public class PicturesServiceTest {
         assertEquals(key, object.getKey());
 
         // 두 번째 사진으로 대체 후 결과 출력
-        picturesList.set(0, picturesTwo);
+        postsPicturesList.set(0, postsPicturesTwo);
 
-        Pictures picturesTwoSavedRepository = picturesRepository.saveAll(picturesList).get(0);
-        Pictures picturesTwoFromRepository = picturesRepository.findAllBySchedules(schedules).get(0);
+        PostsPictures picturesTwoSavedRepository = postsPicturesRepository.saveAll(postsPicturesList).get(0);
+        PostsPictures picturesTwoFromRepository = postsPicturesRepository.findByPosts(posts).get(0);
 
-        assertEquals(picturesTwoSavedRepository.getPicturesURL(), picturesTwoFromRepository.getPicturesURL());
-        System.out.println("최초 저장된 사진 URL: " + picturesTwoSavedRepository.getPicturesURL());
-        System.out.println("추출된 사진 URL: " + picturesTwoFromRepository.getPicturesURL());
+        assertEquals(picturesTwoSavedRepository.getPostsPicturesURL(), picturesTwoFromRepository.getPostsPicturesURL());
+        System.out.println("최초 저장된 사진 URL: " + picturesTwoSavedRepository.getPostsPicturesURL());
+        System.out.println("추출된 사진 URL: " + picturesTwoFromRepository.getPostsPicturesURL());
 
         // S3 두 번째 사진 등록 기능 확인
 
-        String keyTwo = picturesTwoFromRepository.getPicturesName();
+        String keyTwo = picturesTwoFromRepository.getPostsPicturesName();
         String contentTwo = "테스트 사진2";
         amazonS3.putObject(bucketName, keyTwo, contentTwo);
         System.out.println("파일을 업로드하였습니다. 파일 이름=" + keyTwo +", 파일 내용=" + contentTwo);
@@ -233,33 +245,38 @@ public class PicturesServiceTest {
         S3Object objectTwo = amazonS3.getObject(bucketName, keyTwo);
         System.out.println("파일을 가져왔습니다. 파일 이름=" + objectTwo.getKey());
         assertEquals(keyTwo, objectTwo.getKey());
-
     }
 
     @Test
-    void testDeletePictures() {
-        // Mock data and behaviors
+    void testDeletePostsPictures(){
+// Mock data and behaviors
         AmazonS3 amazonS3 = setUpS3();
-        Pictures pictures = MockPictures();
-        Schedules schedules = MockSchedules();
-        List<Pictures> picturesList = schedules.getPicturesList();
-        picturesList.add(pictures);
+        PostsPictures postsPictures = MockPostsPictures();
+        Posts posts = MockPosts();
+        Users users = MockUsers();
+        List<PostsPictures> postsPicturesList = posts.getPostsPicturesList();
+        postsPicturesList.add(postsPictures);
 
-        when(picturesRepository.findAllBySchedules(schedules)).thenReturn(schedules.getPicturesList());
-        when(picturesRepository.saveAll(picturesList)).thenReturn(picturesList);
+        List<Posts> postsList = new ArrayList<>();
+        postsList.add(posts);
 
-        List<Pictures> pictureSavedRepository = picturesRepository.saveAll(picturesList);
+        when(postsPicturesRepository.findByPosts(posts)).thenReturn(posts.getPostsPicturesList());
+        when(postsRepository.findByUsers(users)).thenReturn(postsList);
+        when(postsPicturesRepository.saveAll(postsPicturesList)).thenReturn(postsPicturesList);
 
-        for(Pictures picture: pictureSavedRepository){
-            System.out.println("저장된 사진 이름: " + picture.getPicturesName());
+        List<PostsPictures> pictureSavedRepository = postsPicturesRepository.saveAll(postsPicturesList);
+
+        for(PostsPictures postsPicture: pictureSavedRepository){
+            System.out.println("저장된 사진 이름: " + postsPicture.getPostsPicturesName());
         }
+
 
         // S3 사진 등록 기능 확인
         String bucketName = "foo";
         amazonS3.createBucket(bucketName);
         System.out.println(bucketName +" 버킷 생성");
 
-        String key = pictureSavedRepository.get(0).getPicturesName();
+        String key = pictureSavedRepository.get(0).getPostsPicturesName();
         String content = "테스트 사진1";
         amazonS3.putObject(bucketName, key, content);
         System.out.println("파일을 업로드하였습니다. 파일 이름=" + key +", 파일 내용=" + content);
@@ -267,6 +284,7 @@ public class PicturesServiceTest {
         S3Object object = amazonS3.getObject(bucketName, key);
         System.out.println("파일을 가져왔습니다. 파일 이름=" + object.getKey());
         assertEquals(key, object.getKey());
+
 
         // S3 사진 삭제 기능 확인
         // 사진 삭제 성공시 파일 삭제 메시지 출력, 아닐시 Exception 출력
@@ -282,15 +300,15 @@ public class PicturesServiceTest {
 
         // Repository에서 파일 정보 삭제
 
-        picturesList.clear();
+        postsPicturesList.clear();
 
-        picturesRepository.deleteAll(picturesList);
+        postsPicturesRepository.deleteAll(postsPicturesList);
 
-        if( picturesRepository.findAllBySchedules(schedules).isEmpty()){
+        if( postsPicturesRepository.findByPosts(posts).isEmpty()){
             System.out.println("사진이 모두 삭제되었습니다. ");
         }
 
-        assertTrue(picturesRepository.findAllBySchedules(schedules).isEmpty());
+        assertTrue(postsPicturesRepository.findByPosts(posts).isEmpty());
 
     }
 }
