c034d749683ce1429e25fef02079fa5c4b3be128
Jang-JIye <jjy2646@gmail.com>
2023-11-10 23:47:50 +0900

    fix : 소셜로그인
    - 코드 정리, 신규 가입 안되고 있음

--- src/main/java/com/sparta/team2project/users/social/controller/KakaoController.java
+++ src/main/java/com/sparta/team2project/users/social/controller/KakaoController.java
@@ -2,20 +2,13 @@ package com.sparta.team2project.users.social.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sparta.team2project.commons.Util.JwtUtil;
-import com.sparta.team2project.commons.dto.MessageResponseDto;
 import com.sparta.team2project.commons.security.UserDetailsServiceImpl;
-import com.sparta.team2project.refreshToken.TokenDto;
 import com.sparta.team2project.users.UserService;
-import com.sparta.team2project.users.dto.LoginRequestDto;
-import com.sparta.team2project.users.social.dto.KakaoUserInfoDto;
-import com.sparta.team2project.users.social.dto.ResponseDto;
 import com.sparta.team2project.users.social.service.KakaoService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,9 +29,4 @@ public class KakaoController {
         // 리프레시 토큰을 받기 위해 tokenDto 생성
         return kakaoService.kakaoLogin(code,response);
     }
-// 카카오 아이디
-//@GetMapping("/kakao/callback")
-//public ResponseDto<KakaoUserInfoDto> kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
-//    return kakaoService.kakaoLogin(code, response);
-//}
 }
